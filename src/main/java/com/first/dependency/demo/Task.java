35489d97dc75231781bb0d6d6bfb430d40a84e32
AyeshaKhanProd <ayesha.j.khan1994@gmail.com>
2020-09-02 00:35:26 +0530

    ****Khan

--- src/main/java/com/first/dependency/demo/Task.java
+++ src/main/java/com/first/dependency/demo/Task.java
@@ -2,14 +2,22 @@ package com.first.dependency.demo;
 
 public class Task {
     public static void main(String[] args) {
-        String name ="AyeshaKhan";
-        char[] word = name.toCharArray();
-        for (int i=0;i< word.length;i++){
-            if(i< word.length-4)
-                System.out.print("*");
-            else
-                System.out.print(word[i]);
-        }
+        String name = "Ayesh";
+        System.out.println(mask(name));
+    }
 
+    protected static String mask(String name) {
+        if (name==null||name.equals("")){
+            return "Name can not be null or empty";
+        }
+        char[] word = name.toCharArray();
+        StringBuilder br = new StringBuilder();
+        for (int i = 0; i < word.length; i++) {
+            if (i < word.length - 4)
+                br.append("*");
+            else
+                br.append(word[i]);
+        }
+        return br.toString();
     }
 }
